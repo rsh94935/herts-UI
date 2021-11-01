a5599c1617e090662b095f53125cd7b36f59d5c0
rsh94935 <robert.s.hayward@hotmail.com>
2021-11-01 22:31:04 +0000

    Added API call to obtain the location coords based on given address

--- src/app/car-plan/car-plan.component.ts
+++ src/app/car-plan/car-plan.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Location } from '../classes/location';
 
@@ -9,11 +10,12 @@ import { Location } from '../classes/location';
 export class CarPlanComponent implements OnInit {
   oExpanded: Array<boolean> = [false, false];
   location: string = 'current';
-  coords: Array<number> = [0, 0];
+  startCoords: Array<number> = [0, 0];
+  endCoords: Array<number> = [0, 0];
   current = new Location("","","","","");
   end = new Location("","","","","");
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
 
   ngOnInit(): void {
   }
@@ -30,15 +32,51 @@ export class CarPlanComponent implements OnInit {
 
     if ( event === "current" ) {
       navigator.geolocation.getCurrentPosition((loc) => {
-        this.coords = [loc?.coords?.latitude, loc?.coords?.longitude];
+        this.startCoords = [loc?.coords?.latitude, loc?.coords?.longitude];
       });
+    } else {
     }
   }
 
   public checkCustom(): boolean {
-    console.log(this.location)
     return this.location === "custom";
   }
 
-  public onCustomSubmit(): void {}
+  public onCustomSubmit(): void {
+    let header = {
+      //headers: new HttpHeaders().set("Authorization", "Bearer " + JSON.parse(this.AWS).AccessToken)
+    }
+    let params = {
+      streetAddress: this.current.streetAddress,
+      locality: this.current.locality,
+      adminDistrict: this.current.adminDistrict,
+      adminDistrict2: this.current.adminDistrict2,
+      postCode: this.current.postCode
+    }
+    this.httpClient.post("https://a1fivkgat7.execute-api.eu-west-2.amazonaws.com/dev/getLocation", params, header).subscribe(res => {
+      let test = JSON.stringify(res);
+      let json = JSON.parse(test);
+      let coords = json?.message?.resourceSets[0]?.resources[0]?.point?.coordinates;
+      this.startCoords = [coords[0], coords[1]];
+    });
+  }
+
+  public onEndSubmit(): void {
+    let header = {
+      //headers: new HttpHeaders().set("Authorization", "Bearer " + JSON.parse(this.AWS).AccessToken)
+    }
+    let params = {
+      streetAddress: this.end.streetAddress,
+      locality: this.end.locality,
+      adminDistrict: this.end.adminDistrict,
+      adminDistrict2: this.end.adminDistrict2,
+      postCode: this.end.postCode
+    }
+    this.httpClient.post("https://a1fivkgat7.execute-api.eu-west-2.amazonaws.com/dev/getLocation", params, header).subscribe(res => {
+      let test = JSON.stringify(res);
+      let json = JSON.parse(test);
+      let coords = json?.message?.resourceSets[0]?.resources[0]?.point?.coordinates;
+      this.endCoords = [coords[0], coords[1]];
+    });
+  }
 }
