bcd8b4e0fe04abb92aedbed7c248080539a6e6ed
rsh94935 <robert.s.hayward@hotmail.com>
2021-11-04 00:12:41 +0000

    Setup initial enum file for preferences storage

---
+++ src/app/models/preferences.enum.ts
@@ -0,0 +1,8 @@
+export enum preferences {
+    LandmarksAndHistoricalSites = "Historical Landmarks",
+    Restaurants = "Restaurants",
+    Shop = "Shopping Centers",
+    Supermarkets = "Supermarkets",
+    Attractions = "Activities",
+    SightseeingTours = "Walking Routes"
+}
